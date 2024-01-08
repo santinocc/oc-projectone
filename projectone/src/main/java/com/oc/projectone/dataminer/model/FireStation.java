ed79103b4348e58ed1d2565bae750df72e855350
Santino <santino.cervera@gmail.com>
2024-01-07 21:41:02 -0600

    0. OpenClassrooms Project One Folder Structure and Initial Code Setup

---
+++ projectone/src/main/java/com/oc/projectone/dataminer/model/FireStation.java
@@ -0,0 +1,33 @@
+package com.oc.projectone.dataminer.model;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class FireStation {
+	
+	private Set<String> addresses = new HashSet<>();
+	private String stationNumber;
+	
+	public FireStation(String stationNumber) {
+		this.stationNumber = stationNumber;
+	}
+	
+	public FireStation addAddress(String address) {
+		addresses.add(address);
+		return this;
+	}
+	
+	public String getStationNumber() {
+		return stationNumber;
+	}
+	
+	public Set<String> getAddresses() {
+		return addresses.stream().collect(Collectors.toSet());
+	}
+	
+	@Override
+	public String toString() {
+		return stationNumber.concat(": ") + String.join(", ", addresses);
+	}
+}
